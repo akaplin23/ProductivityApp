756deadc6aad609ef0c43243b0ed48db65e59c44
akaplin23 <akaplin23@gmail.com>
2026-01-18 17:30:07 -0800

    Initial Commit

---
+++ productivity-app/productivity_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  productivity_appApp.swift
+//  productivity-app
+//
+//  Created by Ava Kaplin on 1/18/26.
+//
+
+import SwiftUI
+
+@main
+struct productivity_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
